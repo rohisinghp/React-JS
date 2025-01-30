66d5e9b4d806207b84ed4bcb2b65436159101cde
rohit.singh <rohitsingh94874@gmail.com>
2025-01-30 14:42:47 +0530

    initialize React app with Vite and Tailwind CSS setup and Implement React Router DOM

---
+++ 08reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-orange-700'>React Router DOM</h1>
+    </>
+  )
+}
+
+export default App
