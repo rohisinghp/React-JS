14bc0ba7865eeac01c27244e809546a102df3e57
rohit.singh <rohitsingh94874@gmail.com>
2025-01-31 23:33:19 +0530

    some changes

--- 08reactRouter/src/components/Header/Header.jsx
+++ 08reactRouter/src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link, NavLink} from 'react-router-dom'
+import About from "../About/About";
+import Home from "../Home/Home";
 
 export default function Header() {
     return (
@@ -33,12 +35,48 @@ export default function Header() {
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                <NavLink to="/"
+                                    className={({isActive}) =>
+                                        `${isActive ? "text-orange-700" : "text-black"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
+                                </NavLink>
+                            </li>
+                            
+                            
+                        </ul>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink to="/About"
+                                    className={({isActive}) =>
+                                        `${isActive ? "text-orange-700" : "text-black"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+                            
+                            
+                        </ul>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink to="/Contact"
+                                    className={({isActive}) =>
+                                        `${isActive ? "text-orange-700" : "text-black"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Contact Us
                                 </NavLink>
                             </li>
                             
@@ -50,3 +88,6 @@ export default function Header() {
         </header>
     );
 }
+
+// ${isActive ? "text-orange-700" : "text-gray-50"}
+// ${isActive ? "text-orange-700" : "text-black"}
---
+++ 08reactRouter/src/components/User/User.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function User() {
+
+    const {userid} = useParams();
+    
+
+    return(
+        <>
+         <div className="text-black text-2xl justify-center flex font-bold bg-amber-400">User : {userid}</div>
+         <button class="btn btn-primary">Button</button>
+
+         <div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+         </>
+    )
+
+}
